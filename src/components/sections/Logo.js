a3bc7be267a09e6ef4151d5733da17ae349ec1c3
Simon <simonsaysgameon99@gmail.com>
2022-06-27 13:55:40 +0200

    update

---
+++ src/components/sections/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const LogoText = styled.h1
+font-family: 'Akaya Telivigala';
+
+const Logo = () => {
+  return (
+    <LogoText>
+        EEH Game Show
+    </LogoText>
+  )
+}
+
+export default Logo
